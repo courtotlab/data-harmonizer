80e05f63217ce2d29235ce1fb32a129c7839427c
Ryan Seeto <rseeto@oicr.on.ca>
2025-03-06 11:24:57 -0500

    feat: add field_desc_gen_openai

--- data_harmonizer/synthetic_data.py
+++ data_harmonizer/synthetic_data.py
@@ -60,3 +60,56 @@ def field_name_gen_openai(
             attempt=attempt+1
 
     return result_list
+
+def field_desc_gen_openai(
+        field_desc: str, client: OpenAI = client, model_name: str = 'gpt-4o-mini', num_syn: int = 7
+    ) -> list[str] | None:
+    """Generates a list of synonyms from a given field descriptions using OpenAI
+
+    Parameters
+    ----------
+    field_desc : str
+        The field description to generate synonyms for
+    client : OpenAI, optional
+        Client to connect to OpenAI, by default client
+    model_name : str, optional
+        OpenAI model to use to generate synonyms, by default 'gpt-4o-mini'
+    num_syn : int, optional
+        The number of synonyms to return, by default 7
+
+    Returns
+    -------
+    list[str] | None
+        Python list of synonyms
+    """
+
+    prompt = f"""
+    You are a helpful medical research assistant.
+
+    Create {num_syn} unique field descriptions, of 2-3 sentences each, to be used in a clinical data set that are synonymous with the given field description. Return the result as a list of strings without comments. Exclude the given field description in the list.
+
+    <<<
+    Field description: {field_desc}
+    >>>
+    """
+
+    completion = client.chat.completions.create(
+        model=model_name,
+        store=False,
+        messages=[
+            {'role': 'user', 'content': prompt}
+        ]
+    )
+
+    attempt=1
+    while attempt<=5:
+        try:
+            completion.choices[0].message.content
+            result_list = ast.literal_eval(
+                completion.choices[0].message.content
+            )
+            break
+        except:
+            attempt=attempt+1
+
+    return result_list
