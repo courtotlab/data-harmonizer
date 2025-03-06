20c389ddd3d436a8397b27d512f034e8b2a07060
Ryan Seeto <rseeto@oicr.on.ca>
2025-03-06 10:55:19 -0500

    feat: add field_name_gen_openai

--- data_harmonizer/synthetic_data.py
+++ data_harmonizer/synthetic_data.py
@@ -1,4 +1,5 @@
 import os
+import ast
 from openai import OpenAI
 
 # TODO: load environment variables
@@ -6,3 +7,35 @@ from openai import OpenAI
 client = OpenAI(
   api_key=os.getenv('OPENAI_API_KEY')
 )
+
+def field_name_gen_openai(field_name, client=client, model_name='gpt-4o-mini', num_syn=7):
+    prompt = f"""
+    You are a helpful medical research assistant.
+    
+    Create {num_syn} wholly unique field names, formatted in either snake case and camel case, to be used in a clinical data set that are synonymous with the given field name. Return the result as a list of strings without comments. Exclude the given field name in the list.
+
+    <<<
+    Field name: {field_name}
+    >>>
+    """
+
+    completion = client.chat.completions.create(
+        model=model_name,
+        store=False,
+        messages=[
+            {'role': 'user', 'content': prompt}
+        ]
+    )
+
+    attempt=1
+    while attempt<=5:
+        try:
+            completion.choices[0].message.content
+            result_list = ast.literal_eval(
+                completion.choices[0].message.content
+            )
+            break
+        except:
+            attempt=attempt+1
+
+    return result_list
