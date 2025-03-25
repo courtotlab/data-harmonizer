f8844e3085f3c5ae129f0a5e64d1ce38f11b36dc
Ryan Seeto <rseeto@oicr.on.ca>
2025-03-25 15:53:40 -0400

    feat: function to create triplet template

--- data_harmonizer/split_data.py
+++ data_harmonizer/split_data.py
@@ -1,5 +1,43 @@
+import pandas as pd
+import itertools
+
+def create_triplet_template(schema_df):
+    
+    # list all combinations of 2 items each
+    unique_field_name_combos = list(itertools.combinations(
+        schema_df['field_name'], 2
+    ))
+
+    pos = []
+    neg = []
+    for unique_field_name_combo in unique_field_name_combos:
+        pos.append(schema_df[
+            schema_df['field_name'] == unique_field_name_combo[0]
+        ])
+        neg.append(schema_df[
+            schema_df['field_name'] == unique_field_name_combo[1]
+        ])
+
+    pos_df = pd.concat(pos).add_prefix('pos_').reset_index(drop=True)
+    neg_df = pd.concat(neg).add_prefix('neg_').reset_index(drop=True)
+
+    triplet_template = pd.concat([pos_df, neg_df], axis=1)
+
+    return triplet_template
+
+
+
 def main():
-    pass
+
+    schema_df = get_schema_features()
+
+    schema_df = schema_df[
+        ['field_name', 'field_description']
+    ]
+    
+    triplet_template = create_triplet_template(
+        schema_df
+    )
 
 if __name__=="__main__":
     main()
