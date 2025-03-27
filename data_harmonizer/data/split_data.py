e128c36309d011c99891524f839de428d91013df
Ryan Seeto <rseeto@oicr.on.ca>
2025-03-27 10:24:31 -0400

    fix: change combinations to permutations in create_triplet_template

--- data_harmonizer/data/split_data.py
+++ data_harmonizer/data/split_data.py
@@ -3,19 +3,19 @@ import itertools
 
 def create_triplet_template(schema_df: pd.DataFrame) -> pd.DataFrame:
     
-    # list all combinations of 2 items each
-    unique_field_name_combos = list(itertools.combinations(
+    # list all permutations of 2 items each
+    unique_field_name_perms = list(itertools.permutations(
         schema_df['field_name'], 2
     ))
 
     pos = []
     neg = []
-    for unique_field_name_combo in unique_field_name_combos:
+    for unique_field_name_perm in unique_field_name_perms:
         pos.append(schema_df[
-            schema_df['field_name'] == unique_field_name_combo[0]
+            schema_df['field_name'] == unique_field_name_perm[0]
         ])
         neg.append(schema_df[
-            schema_df['field_name'] == unique_field_name_combo[1]
+            schema_df['field_name'] == unique_field_name_perm[1]
         ])
 
     pos_df = pd.concat(pos).add_prefix('pos_').reset_index(drop=True)
