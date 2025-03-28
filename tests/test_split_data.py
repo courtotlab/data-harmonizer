2e447663aef91d44c01d88d12673837ccb70427a
Ryan Seeto <rseeto@oicr.on.ca>
2025-03-28 15:25:34 -0400

    test: add test for split_data

--- tests/test_split_data.py
+++ tests/test_split_data.py
@@ -1,6 +1,6 @@
 import pytest
 import pandas as pd
-from data_harmonizer.data.split_data import create_triplet_template
+from data_harmonizer.data.split_data import create_triplet_template, split_data
 
 param_create_triplet_template = [
     ( # base example
@@ -83,3 +83,70 @@ def test_create_triplet_template (
     expected = expected.sort_values(by=expected.columns.tolist()).reset_index(drop=True)
 
     assert pd.testing.assert_frame_equal(actual, expected) is None
+
+
+param_split_data = [
+    ( # base example
+        pd.DataFrame(
+            {
+                'field_name': [
+                    'field_name_1', 'field_name_2', 'field_name_3',
+                    'field_name_4', 'field_name_5', 'field_name_6'
+                ],
+                'field_description': [
+                    'field_description_1', 'field_description_2', 'field_description_3',
+                    'field_description_4', 'field_description_5', 'field_description_6'
+                ],
+                'reference_field_name': [
+                    'ref_1', 'ref_1', 'ref_1', 'ref_2', 'ref_2', 'ref_2', 
+                ]
+            }
+        ),
+        {
+            'val': pd.DataFrame(
+                {
+                    'field_name': ['field_name_1', 'field_name_4'],
+                    'field_description': ['field_description_1', 'field_description_4'],
+                    'reference_field_name': ['ref_1', 'ref_2']
+                }
+            ),
+            'test': pd.DataFrame(
+                {
+                    'field_name': ['field_name_2', 'field_name_5'],
+                    'field_description': ['field_description_2', 'field_description_5'],
+                    'reference_field_name': ['ref_1', 'ref_2']
+                }
+            ),
+            'train': pd.DataFrame(
+                {
+                    'field_name': ['field_name_3','field_name_6'],
+                    'field_description': ['field_description_3', 'field_description_6'],
+                    'reference_field_name': ['ref_1', 'ref_2']
+                }
+            )
+        }
+    )
+]
+
+@pytest.mark.parametrize(
+    'synthetic_df_2, dataset_dict_2', 
+    param_split_data
+)
+
+def test_split_data(
+    synthetic_df_2, dataset_dict_2
+):
+    """Test data_harmonizer.data.split_data.split_data"""
+
+    actual_dict = split_data(synthetic_df_2)
+    expected_dict = dataset_dict_2
+
+    for data_type in ['train', 'val', 'test']:
+        actual = actual_dict[data_type]
+        expected = expected_dict[data_type]
+
+        # sort dataframe in case values are present but ordered differently
+        actual = actual.sort_values(by=actual.columns.tolist()).reset_index(drop=True)
+        expected = expected.sort_values(by=expected.columns.tolist()).reset_index(drop=True)
+
+        assert pd.testing.assert_frame_equal(actual, expected) is None
