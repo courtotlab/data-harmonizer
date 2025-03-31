3511bd02cbf7d84ab5cd74e7e56e44239b142dfa
Ryan Seeto <rseeto@oicr.on.ca>
2025-03-31 11:31:10 -0400

    test: add test for create_triplet_df

--- tests/test_split_data.py
+++ tests/test_split_data.py
@@ -1,6 +1,6 @@
 import pytest
 import pandas as pd
-from data_harmonizer.data.split_data import create_triplet_template, split_data
+from data_harmonizer.data.split_data import create_triplet_template, split_data, create_triplet_df
 
 param_create_triplet_template = [
     ( # base example
@@ -150,3 +150,79 @@ def test_split_data(
         expected = expected.sort_values(by=expected.columns.tolist()).reset_index(drop=True)
 
         assert pd.testing.assert_frame_equal(actual, expected) is None
+
+
+param_create_triplet_df = [
+    ( # base example
+        pd.DataFrame(
+            {
+                'field_name': [
+                    'field_name_1', 'field_name_2', 'field_name_3', 'field_name_4'
+                ],
+                'field_description': [
+                    'field_description_1', 'field_description_2',
+                    'field_description_3', 'field_description_4'
+                ],
+                'reference_field_name': ['ref_5', 'ref_5', 'ref_6', 'ref_6']
+            }
+        ),
+        pd.DataFrame(
+            {
+                'pos_field_name': ['ref_5', 'ref_6'],
+                'pos_field_description': [
+                    'field_description_5', 'field_description_6'
+                ],
+                'neg_field_name': [
+                    'field_name_7', 'field_name_8'
+                ],
+                'neg_field_description': [
+                    'field_description_7', 'field_description_8'
+                ],
+                
+            }
+        ),
+        pd.DataFrame(
+            {
+                'field_name': [
+                    'field_name_1', 'field_name_2', 'field_name_3', 'field_name_4'
+                ],
+                'field_description': [
+                    'field_description_1', 'field_description_2',
+                    'field_description_3', 'field_description_4'
+                ],
+                'pos_field_name': ['ref_5', 'ref_5', 'ref_6', 'ref_6'],
+                'pos_field_description': [
+                    'field_description_5', 'field_description_5', 
+                    'field_description_6', 'field_description_6'
+                ],
+                'neg_field_name': [
+                    'field_name_7', 'field_name_7', 
+                    'field_name_8', 'field_name_8'
+                ],
+                'neg_field_description': [
+                    'field_description_7', 'field_description_7', 
+                    'field_description_8','field_description_8'
+                ]
+            }
+        )
+    )
+]
+
+@pytest.mark.parametrize(
+    'synthetic_df_3, triplet_template_3, triplet_df_3', 
+    param_create_triplet_df
+)
+
+def test_create_triplet_df(
+    synthetic_df_3, triplet_template_3,  triplet_df_3
+):
+    """Test data_harmonizer.data.split_data.create_triplet_df"""
+
+    actual = create_triplet_df(synthetic_df_3, triplet_template_3)
+    expected = triplet_df_3
+
+    # sort dataframe in case values are present but ordered differently
+    actual = actual.sort_values(by=actual.columns.tolist()).reset_index(drop=True)
+    expected = expected.sort_values(by=expected.columns.tolist()).reset_index(drop=True)
+
+    assert pd.testing.assert_frame_equal(actual, expected) is None
