09a541484b6682d3130dfa2fa2eefd9e1cd1465a
Ryan Seeto <rseeto@oicr.on.ca>
2025-04-01 15:15:12 -0400

    test: add baseline test for retry_gen_data

---
+++ tests/test_synthetic_data.py
@@ -0,0 +1,31 @@
+import pytest
+import pandas as pd
+from data_harmonizer.data.synthetic_data import retry_gen_data
+
+param_retry_gen_data = [
+    ( # base example
+        '["test"]',
+        1,
+        ["test"]
+    )
+]
+
+@pytest.mark.parametrize(
+    'str_repre_1, num_syn_1, expected_lst_1', 
+    param_retry_gen_data
+)
+
+def test_retry_gen_data(
+    str_repre_1, num_syn_1, expected_lst_1
+):
+    """Test data_harmonizer.data.synthetic_data.retry_gen_data"""
+
+    def llm_call(*args, **kwargs):
+        return str_repre_1
+    
+    actual = retry_gen_data(
+        llm_call, 'pass', num_syn_1, 3
+    )
+    expected = expected_lst_1
+
+    assert actual == expected
