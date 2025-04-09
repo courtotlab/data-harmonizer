0bb4977843476c71e6dee886480340889675854f
Ryan Seeto <rseeto@oicr.on.ca>
2025-04-09 14:37:43 -0400

    docs: add function and module docstring

--- data_harmonizer/data/schema_data.py
+++ data_harmonizer/data/schema_data.py
@@ -1,10 +1,21 @@
-"""Extract features from schema"""
+"""Extract features from linkml schema"""
 
 import pandas as pd
 import yaml
 
-def get_schema_features(linkml_path):
-    """Extract features from schema"""
+def get_schema_features(linkml_path: str) -> pd.DataFrame:
+    """Open a linkml schema file and convert feature columns to dataframe
+
+    Parameters
+    ----------
+    linkml_path : str
+        Path to linkml schema file
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe containing feature columns of interest
+    """
 
     with open(linkml_path, 'r', encoding='utf-8') as file:
         linkml_schema = yaml.safe_load(file)
