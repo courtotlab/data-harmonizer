ab9e1baaea81cc1ee1d81ea39b89e9843b424a56
Ryan Seeto <rseeto@oicr.on.ca>
2025-04-10 15:25:19 -0400

    feat: load target and source schema features

--- data_harmonizer/main.py
+++ data_harmonizer/main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from data_harmonizer.data.schema_data import get_schema_features
+
+load_dotenv()
+
+TARGET_LINKML_PATH = os.getenv('TARGET_LINKML_PATH')
+SOURCE_LINKML_PATH = os.getenv('SOURCE_LINKML_PATH')
+
 def main():
-    pass
+
+    target = get_schema_features(os.path.abspath(
+        os.path.join(os.path.dirname( __file__ ), '..', TARGET_LINKML_PATH)
+    ))
+
+    source = get_schema_features(os.path.abspath(
+        os.path.join(os.path.dirname( __file__ ), '..', SOURCE_LINKML_PATH)
+    ))
 
 if __name__ == '__main__':
     main()
